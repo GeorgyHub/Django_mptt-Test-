126651d109ffb72c4cc17f6161a93d062519ce94
Georgy <83170949+GeorgyHub@users.noreply.github.com>
2024-03-02 13:18:24 +0300

    edit urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from blog.views import index
+from blog.views import CategoryListView, PostByCategoryView
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', index),
+    path('', CategoryListView.as_view(), name='category-list'),
+    path('<str:slug>/', PostByCategoryView.as_view(), name='post-by-category'),
 ]
